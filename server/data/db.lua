02fabcbb4de974fbe2f9beaed55de67245d41b5a
JericoFX <56100605+JericoFX@users.noreply.github.com>
2023-08-22 14:09:43 -0300

    return the table!

--- server/data/db.lua
+++ server/data/db.lua
@@ -5,10 +5,15 @@ local QUERY_STRINGS = {
     ]]
 }
 
+--- Function to grab the player by citizenid IF the player isnt connected
+---@param id string - Citizenid from the player
+---@return {}
 function db.GrabByCitizenID(id)
-    if not id then return end
+    if not id then return false end
     --https://overextended.github.io/docs/oxmysql/Usage/single
     local variable = MySQL.single.await(QUERY_STRINGS.GrabPlayerByCitizenId, {id})
     if not variable or #variable == 0 or table.type(variable) == "empty" then return false end
     return variable
 end
+
+return db
