96a3a75240abb78fd23666df231a4a956ed54530
JericoFX <56100605+JericoFX@users.noreply.github.com>
2023-08-22 14:00:32 -0300

    return some data from the server, i dont know what information i needed  it now

---
+++ server/data/db.lua
@@ -0,0 +1,13 @@
+local db = {}
+local QUERY_STRINGS = {
+    GrabPlayerByCitizenId = [[
+        SELECT JSON_VALUE(player.charinfo,'$.firstname') AS firstname, JSON_VALUE(player.charinfo,'$.lastname') AS lastname,JSON_VALUE(player.charinfo,'$.phone') AS phone,citizenid FROM `player` WHERE `citizenid` = ? LIMIT 1
+    ]]
+}
+
+function db.GrabByCitizenID(id)
+    --https://overextended.github.io/docs/oxmysql/Usage/single
+    local variable = MySQL.single.await(QUERY_STRINGS.GrabPlayerByCitizenId, {id})
+    if not variable or #variable == 0 or table.type(variable) == "empty" then return false end
+    return variable
+end
--- server/init.lua
+++ server/init.lua
@@ -1,19 +1,25 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
-
+local db = require "server.data.db"
 lib.callback.register("fx::pmi::server::getPlayerInfo",function(source,id) 
     if not source or not id then return end
-    local Player = QBCore.Functions.GetPlayer(source)
-    if not Player or not Config.Job[Player.PlayerData.job.name] then return end
+    local PlayerData in QBCore.Functions.GetPlayer(source)
+    if not PlayerData or not Config.Job[PlayerData.job.name] then return end
     local _OPlayer = QBCore.Functions.GetPlayerByCitizenId(id)
     if  _OPlayer then -- player is Online
         ---TODO return all the that i need from the Player table.
-
+        return {
+            firstname = PlayerData?.charinfo.firstname,
+            lastname = PlayerData?.charinfo.lastname,
+            phone = PlayerData?.charinfo.phone,
+            citizenid = PlayerData?.citizenid,
+        }
     else
         -- Player is not online
         -- https://overextended.dev/oxmysql/Functions/single
         -- This function does nothing because QB doesnt have that sql structure.
         ---TODO return all the that i need from the Player table.
-        local _CurrentPlayer = MySQL.single.await('SELECT `firstname`, `lastname` FROM `users` WHERE `citizenid` = ? LIMIT 1', {id})
+        local _CurrentPlayer = db.GrabByCitizenID(id)
         if not _CurrentPlayer then return false end
+        return _CurrentPlayer
     end
 end)
