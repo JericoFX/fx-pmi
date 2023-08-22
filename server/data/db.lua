8ed6a908a16ebb229515df55cee2852748107d6c
JericoFX <56100605+JericoFX@users.noreply.github.com>
2023-08-22 14:01:15 -0300

    check if id is not nil

--- server/data/db.lua
+++ server/data/db.lua
@@ -6,6 +6,7 @@ local QUERY_STRINGS = {
 }
 
 function db.GrabByCitizenID(id)
+    if not id then return end
     --https://overextended.github.io/docs/oxmysql/Usage/single
     local variable = MySQL.single.await(QUERY_STRINGS.GrabPlayerByCitizenId, {id})
     if not variable or #variable == 0 or table.type(variable) == "empty" then return false end
