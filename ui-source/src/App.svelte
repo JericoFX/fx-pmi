568b55d8fa8c22ff01c1d9b86764aa65a7b2b7a9
JericoFX <blackjordison@gmailcom>
2023-08-22 22:50:46 -0300

    update ui

--- ui-source/src/App.svelte
+++ ui-source/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { Button } from 'yesvelte';
+  import { Button, Icon,Tooltip } from "yesvelte";
+    import Officers from "./pages/Officers.svelte";
 </script>
 
 <svelte:head>
@@ -9,7 +10,29 @@
   />
 </svelte:head>
 <main class=" w-screen h-screen relative">
-  <div id="mainFrame" class="w-45vw h-70vh bg-dark" />
+  <div id="mainFrame" class="w-65vw h-80vh bg-#242424 absolute-center rounded">
+    <div id="leftBar" class="absolute left-0 w-4vw h-full bg-#343434 rounded shadow-black shadow-md">
+      <div class="flex h-full justify-around items-center flex-col">
+        <Icon name="shield-check-filled" class="hover:w-9 transition-width cursor-pointer" color="light" size="2x" />
+        <Tooltip placement="top">Officers</Tooltip>
+        <Icon name="user" color="light" class="hover:w-9 transition-width cursor-pointer" size="2x" />
+         <Tooltip placement="top">People</Tooltip>
+        <Icon name="car" color="light" class="hover:w-9 transition-width cursor-pointer" size="2x" />
+        <Tooltip placement="top">Cars</Tooltip>
+        <Icon name="sos" color="light" class="hover:w-9 transition-width cursor-pointer" size="2x" />
+        <Tooltip placement="top">Danger</Tooltip>
+        <Icon name="sword" color="light" class="hover:w-9 transition-width cursor-pointer" size="2x" />
+        <Tooltip placement="top">Weapons</Tooltip>
+        <Icon name="lock-check" color="light" class="hover:w-9 transition-width cursor-pointer" size="2x" />
+        <Tooltip placement="top">Config</Tooltip>
+      </div>
+    </div>
+    <div class="app bg-light absolute right-0 w-94% h-full -z-1">
+    
+            <Officers ></Officers>
+  
+    </div>
+  </div>
 </main>
 
 <style>
